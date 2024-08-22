dc037695dfa03a7bba71c7532614d79a59a58658
Md Habibullah Mia <48855480+mdhabibullah1@users.noreply.github.com>
2024-08-22 20:04:13 +0600

    simple navbar done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { IoMenu } from "react-icons/io5";
+import './navbar.css'
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   const links = (
@@ -10,15 +13,33 @@ const Navbar = () => {
     <li><NavLink to={"/Product"}>Product</NavLink></li>
     </>
   )
+  const [toggle,settoggle]=useState(true)
+
+  const handlemenu = ()=>{
+    settoggle(!toggle)
+  }
+
     return (
-        <div>
+        <div className='flex justify-between relative p-2 items-center max-w-[1800px] mx-auto bg-slate-300'>
           <div>
             <h2 className='text-2xl font-bold'>logo</h2>
           </div>
           <div>
-            <ul>
+            <ul className='md:flex gap-6 hidden '>
                {links}
             </ul>
+            <div onClick={handlemenu} className='text-2xl md:hidden'>
+              {
+                 toggle ? <IoMenu />:<RxCross2 />
+              }
+            
+            </div>
+          </div>
+          <div className={`absolute md:hidden top-12 ${toggle?"-right-24":"right-0"} bg-slate-300 p-4`}>
+            <ul className='space-y-2'>
+              {links}
+            </ul>
+
           </div>
         </div>
     );
